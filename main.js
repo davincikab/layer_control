918664d807827aa46a075306f03cd92e0da8f468
davincikab <davidnganganjeri079@gmail.com>
2021-05-06 23:21:35 -0700

    transit layer

--- main.js
+++ main.js
@@ -2,13 +2,37 @@ mapboxgl.accessToken = 'pk.eyJ1IjoicGlyb21hIiwiYSI6ImNqeWxzNDN3cTAzMzYzb3F3enlvc
 var map = new mapboxgl.Map({
     container: 'map',
     style:'mapbox://styles/mapbox/dark-v10',
-    center: [-7.594081153831553, 14.730749011074153], // master center
-    zoom: 2, // master zoom
+    center: [-122.067310000256271, 38.247539999971082], // master center
+    zoom: 10, // master zoom
     attribution:false,
 });
 
 map.on("load", function(e) {
-    
+    map.addSource('route', {
+        type:'geojson',
+        data:'layers/route.geojson'
+    });
+
+    map.addLayer({
+        id:'transit-route',
+        source:'route',
+        type:'line',
+        paint:{
+            'line-color':'green',
+            'line-width':3
+        },
+        layout:{
+            'visibility':'none'
+        }
+    });
+
+    // city boundary
+    map.addSource("city-boundary", {
+        type:'geojson',
+        data:
+    })
+    // census block
+
 });
 
 var layers = {
@@ -19,7 +43,7 @@ var layers = {
     'Average AM Peak Trips':'average-am-trip',
     'Employee Count':'employee',
     'Unemployment Rate':'unemployment-rate',
-    'Transit Route':'transit_route',
+    'Transit Route':'transit-route',
     'Census Block':'census-block',
     'City Boundary':'city-boundary'
 };
@@ -30,28 +54,32 @@ class LayerControl {
     }
 
     toggleLayer(layerId, checked) {
+        console.log("toggling layer");
+        console.log(layerId);
+
         var visibility = checked ? 'visible' : 'none';
-        map.getSource(layerId) ? map.setLayoutProperty(layerId, 'visibility', visibility) : false;
+        this._map.getLayer(layerId) ? this._map.setLayoutProperty(layerId, 'visibility', visibility) : false;
     }
 
     onAdd(map) {
         this._map = map;
 
         this._container = document.createElement("div");
-        this._container.className = 'mapboxgl-ctrl layer_control';
+        this._container.className = 'mapboxgl-ctrl layer-control';
 
         Object.keys(this.layers).forEach(key => {
             let layer = this.layers[key];
 
             let layerDiv = document.createElement("div");
-            let layerCheckbox = document.createElement("input");
 
+            let layerCheckbox = document.createElement("input");
             layerCheckbox.type = "checkbox";
+            layerCheckbox.className = "layer-element";
             layerCheckbox.value = layer;
             layerCheckbox.id = layer;
 
             layerDiv.append(layerCheckbox);
-            layerDiv.innerHTML = "<label>"+ layer +"</label>"
+            layerDiv.innerHTML += "<label>"+ key +"</label>"
 
             this._container.append(layerDiv);
         });
